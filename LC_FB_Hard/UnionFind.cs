273ff168819c63c4423c93f43805caf26c17c28c
Karthik Hariharan <35323078+lets-bop@users.noreply.github.com>
2018-09-21 09:59:04 -0700

    More changes

---
+++ LC_FB_Hard/UnionFind.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LC_FB_Hard
+{
+    public class UnionFind
+    {
+        internal class Node
+        {
+            internal int Data;
+            internal Node Parent;
+            internal int Depth;
+        }
+        
+        public void MakeSet()
+        {
+
+        }
+
+        public int FindSet()
+        {
+
+        }        
+
+        public void Union()
+        {
+
+        }
+    }    
+}
