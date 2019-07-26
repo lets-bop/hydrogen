2e64b85a73cbb403387a46a7b1687fbfce637845
Karthik Hariharan <karthiks.hariharan@gmail.com>
2019-07-26 14:46:16 -0700

    Update QueueReconstruction.cs

--- LC_FB_Medium/QueueReconstruction.cs
+++ LC_FB_Medium/QueueReconstruction.cs
@@ -4,9 +4,17 @@ using System.Text;
 
 namespace LC_FB_Medium
 {
+    /*
+     * Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. 
+     * Write an algorithm to reconstruct the queue. 
+     * Note: The number of people is less than 1,100.
+     * Example
+     * Input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+     * Output: [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+     * */
     class QueueReconstruction
     {
-
+        // https://www.programcreek.com/2015/03/leetcode-queue-reconstruction-by-height-java/
         public int[][] ReconstructQueue(int[][] people)
         {
             var n = people.Length;
@@ -19,10 +27,7 @@ namespace LC_FB_Medium
 
             foreach (var person in people)
             {
-                var height = person[0];
-                var index = person[1];
-
-                result.Insert(index, person);
+                result.Insert(person[1], person);
             }
 
             return result.ToArray();
