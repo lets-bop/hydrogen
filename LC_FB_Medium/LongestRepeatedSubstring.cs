67f81fd6fb4f053520b649ca7f06d0d0956cd87a
Kaks <kaks@Kakss-MacBook-Pro.local>
2019-10-04 09:25:35 -0700

    Format

--- LC_FB_Medium/LongestRepeatedSubstring.cs
+++ LC_FB_Medium/LongestRepeatedSubstring.cs
@@ -9,6 +9,16 @@ namespace LC_FB_Medium
             int longest = int.MinValue;
             List<string> suffixes = new List<string>();
 
+            // Idea: generate suffix arrays and sort them and run lcp on adjacent pairs.
+            // suffix array of a string starting at index 0 is all characters of the string
+            // suffix array of a string starting at index 1, is the all characters except the first char and so on.
+            // Max of N substring calls and N lcp calls
+            // Empirical analysis confirmed O(N lg N) time until 2012 in Java. Why?
+            // Until 2012, a substring call would not copy characters. It would just refer to the same underlying string
+            // and limit access beginning from the start to the end characters. Just references.
+            // After 2012, they started copying the characters with every substring call. This requires linear time and space.
+            // To fix this, implement your own implementation of the old string class,
+            // implement a CompareTo to enable sorting
             for (int i = 0; i < S.Length; i++) suffixes.Add(S.Substring(i));
             suffixes.Sort();
 
